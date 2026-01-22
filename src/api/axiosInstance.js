75cfbcd0c18453e3de869e1bfa43350d3849194b
Varshaps07 <varshasonnad28@gmail.com>
2026-01-22 19:25:38 +0530

    finallly commiiiiiit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5010/api",
-    // baseURL: "https://interioverse-designs-backend.onrender.com/api",
+  // baseURL: "http://localhost:5010/api",
+    baseURL: "https://interioverse-designs-backend.onrender.com/api",
             
   withCredentials: true,
   headers: {
